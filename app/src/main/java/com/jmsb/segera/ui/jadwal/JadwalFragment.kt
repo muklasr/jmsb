4a9cc460ee8c2f8ce125c8e99cdd56f0255c20b9
muklasr <muclasrachmanto@gmail.com>
2019-10-31 21:36:55 +0700

    lali

--- app/src/main/java/com/jmsb/segera/ui/jadwal/JadwalFragment.kt
+++ app/src/main/java/com/jmsb/segera/ui/jadwal/JadwalFragment.kt
@@ -1,20 +1,29 @@
 package com.jmsb.segera.ui.jadwal
 
+import android.content.Context
 import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.annotation.IntegerRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.jmsb.segera.R
 import com.jmsb.segera.adapter.JadwalAdapter
 import com.jmsb.segera.helper.DataHelper
 import com.jmsb.segera.model.Jadwal
+import kotlinx.android.synthetic.main.add_bottom_sheet.*
+import kotlinx.android.synthetic.main.add_bottom_sheet.view.*
 import kotlinx.android.synthetic.main.fragment_absen.view.*
+import kotlinx.android.synthetic.main.fragment_absen.view.rvJadwal
+import kotlinx.android.synthetic.main.fragment_jadwal.view.*
 import java.util.*
 import kotlin.collections.ArrayList
 import java.text.SimpleDateFormat
@@ -23,7 +32,8 @@ import java.text.SimpleDateFormat
 class JadwalFragment : Fragment() {
 
     private lateinit var dashboardViewModel: JadwalViewModel
-    val list: ArrayList<Jadwal> = ArrayList()
+    var list: ArrayList<Jadwal> = ArrayList()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,14 +44,24 @@ class JadwalFragment : Fragment() {
             ViewModelProviders.of(this).get(JadwalViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_jadwal, container, false)
 
+        root.rootView.btnAdd.setOnClickListener {
+            showBottomSheetDialog(root.context)
+        }
+
+        showRecycler(root)
+
+        return root
+    }
+
+    private fun showRecycler(root: View) {
         val dataHelper = DataHelper(root.context)
         val db = dataHelper.readableDatabase
 
         val c = Calendar.getInstance()
         val df = SimpleDateFormat("W")
         val currentTime = df.format(c.time)
-        val hari = Integer.valueOf(currentTime) -1
-        val cursor: Cursor = db.rawQuery("SELECT * FROM jadwal WHERE hari="+hari, null)
+        val hari = Integer.valueOf(currentTime) - 1
+        val cursor: Cursor = db.rawQuery("SELECT * FROM jadwal WHERE hari=" + hari, null)
         cursor.moveToFirst()
         for (i in 0 until cursor.count step 1) {
             cursor.moveToPosition(i)
@@ -61,6 +81,12 @@ class JadwalFragment : Fragment() {
         db.close()
         root.rootView.rvJadwal.layoutManager = LinearLayoutManager(root.context)
         root.rootView.rvJadwal.adapter = JadwalAdapter(root.context, list)
-        return root
+    }
+
+    private fun showBottomSheetDialog(context: Context) {
+        val view = layoutInflater.inflate(R.layout.add_bottom_sheet, null)
+        val dialog = BottomSheetDialog(context)
+        dialog.setContentView(view)
+        dialog.show()
     }
 }
